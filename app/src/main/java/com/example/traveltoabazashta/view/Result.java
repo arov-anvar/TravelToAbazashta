4f84b10d23ab03b075206cdf08cf36f205ae0d42
arov-anvar <anvarsv963@gmail.com>
2019-10-02 23:01:18 +0300

    update chart in result

--- app/src/main/java/com/example/traveltoabazashta/view/Result.java
+++ app/src/main/java/com/example/traveltoabazashta/view/Result.java
@@ -3,6 +3,7 @@ package com.example.traveltoabazashta.view;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.traveltoabazashta.R;
 import com.github.mikephil.charting.charts.PieChart;
@@ -14,12 +15,14 @@ import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Result extends AppCompatActivity {
+import static com.github.mikephil.charting.utils.ColorTemplate.rgb;
 
-    private PieChart mPieChart;
+public class Result extends AppCompatActivity {
 
     private static final float TRANSPARENT_CIRCLE_RADIUS = 65f;
     private static final int ANIMATION_VALUE = 2000;
@@ -30,9 +33,16 @@ public class Result extends AppCompatActivity {
     private static final String NO_RIGHT = "Не правильно";
     private static final String EMPTY_STRING = "";
 
-    private final int COUNT_RIGHT = 25;
-
+    private final int COUNT_ANSWER = 25;
+    private PieChart mPieChart;
     private int rightAnswer;
+
+    private TextView textViewRight;
+    private TextView textViewLose;
+
+    public static final int[] RED_COLOR = {
+            rgb("#00ff7f"), rgb("#c70000")
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +50,15 @@ public class Result extends AppCompatActivity {
         setContentView(R.layout.activity_result);
         rightAnswer = getIntent().getIntExtra("rightAnswer", 0);
         showPieChart();
+        init();
 
+        textViewRight.setText("Правильных: " + rightAnswer);
+        textViewLose.setText("Не правильных: " + (COUNT_ANSWER - rightAnswer));
+    }
+
+    public void init() {
+        textViewLose = (TextView) findViewById(R.id.count_lose_answer);
+        textViewRight = (TextView) findViewById(R.id.count_right_answer);
     }
 
     public void showPieChart() {
@@ -49,14 +67,16 @@ public class Result extends AppCompatActivity {
         mPieChart.setDrawHoleEnabled(true);
         mPieChart.setTransparentCircleRadius(TRANSPARENT_CIRCLE_RADIUS);
         mPieChart.setHoleRadius(TRANSPARENT_CIRCLE_RADIUS);
-        mPieChart.setDescription(new Description());
         mPieChart.animateXY(ANIMATION_VALUE, ANIMATION_VALUE);
+        Description description = new Description();
+        description.setText("");
+        mPieChart.setDescription(description);
 
         List<PieEntry> yvalues = new ArrayList<>();
-        yvalues.add(new PieEntry(rightAnswer, BUNDLE_KEY_ZERO_INDEX));
-        yvalues.add(new PieEntry(COUNT_RIGHT, BUNDLE_KEY_SECOND_INDEX));
+        yvalues.add(new PieEntry(rightAnswer, 0)); // правильные ответы
+        yvalues.add(new PieEntry(COUNT_ANSWER - rightAnswer, 1)); // неправильные ответы
         PieDataSet dataSet = new PieDataSet(yvalues, EMPTY_STRING);
-        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        dataSet.setColors(RED_COLOR);
 
         ArrayList<String> xVals = new ArrayList<String>();
         xVals.add(RIGHT);
